ca8dcafe008646488818cd47caf8b703b1e44fb6
Jaime Lopez <detec.azul@gmail.com>
2018-03-12 16:06:52 +0000

    Part C -Library Class- completed

---
+++ Library/library.rb
@@ -0,0 +1,34 @@
+class Library
+  attr_reader :books
+  def initialize(books)
+    @books = books
+  end
+
+  def get_book_information_by_title(book_title)
+    for book in @books
+      return book if book[:title] == book_title
+    end
+    return nil
+  end
+
+  def get_book_rental_details_by_title(book_title)
+    book = get_book_information_by_title(book_title)
+    return book[:rental_details]
+  end
+
+  def add_book(new_book)
+    @books << {
+      title: new_book,
+      rental_details: {
+        student_name: "",
+        date: ""
+      }
+    }
+  end
+
+  def rent_book(book_title, student_name, due_date)
+    rental_details = get_book_rental_details_by_title(book_title)
+    rental_details[:student_name] = student_name
+    rental_details[:date] = due_date
+  end
+end
--- Library/specs/library_spec.rb
+++ Library/specs/library_spec.rb
@@ -1,9 +1,98 @@
 require 'minitest/autorun'
-require_relative '../classes'
+require_relative '../library'
 
 class LibraryTests < MiniTest::Test
   def setup
+    @tollcross = Library.new([
+      {
+        title: "lord_of_the_rings",
+        rental_details: {
+            student_name: "Jeff",
+            date: "01/12/16"
+          }
+        },
+        {
+          title: "the_hobbit",
+          rental_details: {
+              student_name: "Anthony",
+              date: "30/01/17"
+            }
+          },
+          {
+            title: "harry_potter",
+            rental_details: {
+                student_name: "Pax Carlton",
+                date: "28/02/18"
+              }
+            },
+            {
+              title: "learn_ruby",
+              rental_details: {
+                  student_name: "Pax Carlton",
+                  date: "28/03/18"
+                }
+              },
+      ])
   end
 
+  def test_books_getter
+    result =  @tollcross.books()
+    expected = {
+      title: "learn_ruby",
+      rental_details: {
+          student_name: "Pax Carlton",
+          date: "28/03/18"
+        }
+      }
+    assert_equal(expected,result.last)
+  end
 
+  def test_get_book_information_by_title()
+    result = @tollcross.get_book_information_by_title("lord_of_the_rings")
+    expected = {
+      title: "lord_of_the_rings",
+      rental_details: {
+          student_name: "Jeff",
+          date: "01/12/16"
+        }
+      }
+    assert_equal(expected,result)
+  end
+
+  def test_get_book_information_by_title__not_found()
+    result = @tollcross.get_book_information_by_title("learn_java")
+    assert_nil(result)
+  end
+
+  def test_get_book_rental_details_by_title()
+    result = @tollcross.get_book_rental_details_by_title("lord_of_the_rings")
+    expected = {
+      student_name: "Jeff",
+      date: "01/12/16"
+    }
+    assert_equal(expected,result)
+  end
+
+  def test_add_book()
+    @tollcross.add_book("learn_java")
+    expected = {
+      title: "learn_java",
+      rental_details: {
+        student_name: "",
+        date: ""
+      }
+    }
+    result = @tollcross.get_book_information_by_title("learn_java")
+    assert_equal(expected,result)
+  end
+
+  def test_rent_book()
+    @tollcross.rent_book("harry_potter","Jaime Lopez","12/03/2018")
+    expected = {
+      student_name: "Jaime Lopez",
+      date: "12/03/2018"
+    }
+    result = @tollcross.get_book_rental_details_by_title("harry_potter")
+    assert_equal(expected,result)
+  end
 end
